b3a016a124f9916d9cac068a497dc56ebe7a7f0a
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2020-12-12 09:20:18 +0800

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { AddItemFormComponent } from './pages/add-item-form/add-item-form.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    AddItemFormComponent
+    AddItemFormComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
