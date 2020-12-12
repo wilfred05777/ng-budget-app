6e8879b0947b0379b7bc73e4f5b6dc65f13d6ef3
Wilfred Erdo <wilfred05777@users.noreply.github.com>
2020-12-12 08:56:59 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { AddItemFormComponent } from './pages/add-item-form/add-item-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    AddItemFormComponent
   ],
   imports: [
     BrowserModule,
